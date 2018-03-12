6c94ca246265b8d76fe2cb59cc87fef992aa704d
James <jamesterrien95@hotmail.fr>
2018-03-12 15:51:35 +0100

    test db

--- src/persistent/ConnectionBD.java
+++ src/persistent/ConnectionBD.java
@@ -1,6 +1,24 @@
 package persistent;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public abstract class ConnectionBD {
 
+        public static void main(String[] args) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                System.out.println("Driver O.K.");
 
+                String url = "jdbc:postgresql://localhost:5432/anikiDB";
+                String user = "postgres";
+                String passwd = "projetpromo";
+
+                Connection conn = DriverManager.getConnection(url, user, passwd);
+                System.out.println("Connexion effective !");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 }
