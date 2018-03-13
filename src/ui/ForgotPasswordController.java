c58ed0840f91c95ba5af86ecfe459c351bde661e
michelexe <enzo.fabre@orange.fr>
2018-03-14 00:12:25 +0100

    Problèmes DB fixés

---
+++ src/ui/ForgotPasswordController.java
@@ -0,0 +1,22 @@
+package ui;
+
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class ForgotPasswordController {
+
+    public void mailKeyEntered(KeyEvent keyEvent) {
+    }
+
+    public void mailClick(MouseEvent mouseEvent) {
+    }
+
+    public void buttonLoginClick(MouseEvent mouseEvent) {
+    }
+
+    public void buttonLoginMouseEntered(MouseEvent mouseEvent) {
+    }
+
+    public void buttonLoginMouseExited(MouseEvent mouseEvent) {
+    }
+}
