0f7cf124f9793e52fe93eebdd8999d7522c360a0
michelexe <enzo.fabre@orange.fr>
2018-03-15 10:43:22 +0100

    ajout de commentaire

--- src/logs/ConnectionLog.java
+++ src/logs/ConnectionLog.java
@@ -1,9 +1,9 @@
 package logs;
 
 public class ConnectionLog {
-    private final static String url ="jdbc:postgresql://localhost:5432/anikidb";
-    private final static String user= "postgres";
-    private final static String pwd ="";
+    private final static String url ="jdbc:postgresql://localhost:5432/anikidb"; // your db name
+    private final static String user= "postgres"; // user name to conect to your db
+    private final static String pwd =""; // password to connect to your database
 
 
     public static String getUrl(){
