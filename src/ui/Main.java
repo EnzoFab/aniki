9dca420c71732c577f41cd8eb7a507ad28811072
michelexe <enzo.fabre@orange.fr>
2018-03-29 01:48:47 +0200

    Page d'accueil

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
